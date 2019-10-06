c0acd31d67e7292399b26b6ca58b3a809c88dd8f
Alex <amscad@users.noreply.github.com>
2019-10-06 17:12:07 +0100

    Setup vim on the machine

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#remove old links (assuming they are links)
+rm -rf ~/.vim
+rm ~/.vimrc
+
+mkdir -p ~/settings/vim
+git clone https://github.com/amscad/vim.git ~/settings/vim
+
+#setup new links to use
+#mkdir -p ~/Dropbox/settings/vim
+ln -s ~/settings/vim ~/.vim
+ln -s ~/settings/vim/vimrc ~/.vimrc
+
+#install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/amscad/vim-plug/master/plug.vim
+    
+vim +PlugInstall +qall 
+
+~/settings/vim/plugged/YouCompleteMe/install.py
