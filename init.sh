dd7a1e86e69d392eb57ad44d3a538b3ade0a414d
amscad <ajmansfield@gmail.com>
2019-10-20 15:31:37 +0100

    Added init script for basic setup

---
+++ init.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+echo "==> Updating and upgrading packages ..."
+
+Add third party repositories
+add-apt-repository ppa:keithw/mosh-dev -y
+add-apt-repository ppa:jonathonf/vim -y
+
+apt-get update
+apt-get upgrade -y
+
+# sudo apt-get install -y \
+#   apache2 \
+#   build-essential \
+#   mosh
+
+sudo apt-get install -qq \
+  apache2 \
+  build-essential \
+  cmake \
+  curl \
+  gdb \
+  git \
+  hugo \
+  jq \
+  mosh \
+  openssh-server \
+  python3 \
+  python3-dev \
+  python3-flake8 \
+  python3-pip \
+  python3-setuptools \
+  python3-venv \
+  python3-wheel \
+  silversearcher-ag \
+  tmux \
+  tree \
+  unzip \
+  wget \
+  --no-install-recommends
+
+
+cat <<EOF > /var/www/html/index.html
+<html><body><h1>Hello World</h1>
+<p>This should install mosh!</p>
+</body></html>
+EOF
+
+git clone  https://github.com/amscad/cloud-shell /home/alex/cloud-shell
+cd /home/alex/cloud-shell
+./setup.sh
