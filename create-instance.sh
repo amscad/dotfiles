edb83a09b07978ee35aa58766b6077b31ca1823f
Alex <amscad@users.noreply.github.com>
2019-10-25 18:06:54 +0100

    Move delete instance after initialising env variables

--- create-instance.sh
+++ create-instance.sh
@@ -5,14 +5,15 @@ gcloud config set project $PROJECT_ID
 export NETWORK="default-allow-ssh-mosh"
 gcloud compute firewall-rules create $NETWORK --allow tcp:22,udp:60000-65000
 
-gcloud compute instances delete $INSTANCE_NAME --zone=$ZONE
-
 export INSTANCE_NAME="devinstance"
 export ZONE="europe-west2-a"
 export IMAGE_FAMILY="ubuntu-1804-lts"
 export IMAGE_PROJECT="ubuntu-os-cloud"
 export INSTANCE_TYPE="n1-standard-1"
 export STARTUP_SCRIPT_URL="gs://dev-machine/init.sh"
+
+gcloud compute instances delete $INSTANCE_NAME --zone=$ZONE
+
 gcloud compute instances create $INSTANCE_NAME \
         --zone=$ZONE \
         --image-family=$IMAGE_FAMILY \
