5a0d7838016d323fe5a22b46db92920af2066df5
amscad <ajmansfield@gmail.com>
2019-12-24 12:39:01 +0000

    Add checks and installs for ohmyzsh and gcloud

--- ish-setup.sh
+++ ish-setup.sh
@@ -1,3 +1,4 @@
+# standard tools
 apk add python3 \
   	ruby \
 	vim \
@@ -12,3 +13,23 @@ apk add python3 \
 	sqlite \
 	jq \
 	mosh
+
+# google cloud shell
+# but only if it has not already been downloaded
+if [ ! -f "$HOME/google-cloud-sdk.tar.gz" ]; then
+#wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.0-linux-x86_64.tar.gz
+	curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.0-linux-x86_64.tar.gz -o $HOME/google-cloud-sdk.tar.gz
+fi
+
+# install ohmyzsh - a useful set of tools for zsh
+# but only if it has not been installed already
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# neovim, using a "common" linux image
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+chmod u+x nvim.appimage
+
+# for current logged user
+#sudo chsh -s /bin/zsh "$USER"
