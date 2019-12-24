36477744763287d1ff8bee12af91fefb71f2200c
Alex <amscad@users.noreply.github.com>
2019-12-24 16:38:07 +0200

    Copy custom folder, and reload zsh

--- update-zsh.sh
+++ update-zsh.sh
@@ -1,4 +1,6 @@
 # copy custom themes and plugins to .oh-my-zsh, if it has been installed
 if [ -d "$HOME/.oh-my-zsh" ]; then
-	cp -r ohmyzsh/custom $HOME/.oh-my-zsh
+	cp -r $HOME/dotfile/ohmyzsh/custom $HOME/.oh-my-zsh
+        cp $HOME/dotfiles/zshrc $HOME/.zshrc
+        source $HOME/.zshrc
 fi
