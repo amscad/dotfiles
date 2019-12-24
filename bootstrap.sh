ea36802a0489168db2c4ec64ae083c9c4c4a7dad
amscad <ajmansfield@gmail.com>
2019-12-24 07:21:07 +0000

    Undo change to save the pinkie - to much muscle memory

--- bootstrap.sh
+++ bootstrap.sh
@@ -97,8 +97,16 @@ else
   echo "ripgrep may, or may not have been updated. read the output messages"
 fi
 
-if ! [ -x "$(command -v hub)" ]; then
- sudo snap install google-cloud-sdk --classic
+if [ ! -d "($HOME)/google-cloud-sdk" ]; then
+  echo " ==> Installing google cloud sdk"
+  # Add the Cloud SDK distribution URI as a package source
+  echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+  # Import the Google Cloud Platform public key
+  curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+  # Update the package list and install the Cloud SDK
+  sudo apt-get update && sudo apt-get install google-cloud-sdk
 fi
 
 if [ ! -d "${HOME}/.fzf" ]; then
