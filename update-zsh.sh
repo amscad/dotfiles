8cac2085be865bc7a5d2a4f121a9307103e46ee0
amscad <ajmansfield@gmail.com>
2019-12-24 14:46:45 +0000

    Fixed typo

--- update-zsh.sh
+++ update-zsh.sh
@@ -1,6 +1,6 @@
 # copy custom themes and plugins to .oh-my-zsh, if it has been installed
 if [ -d "$HOME/.oh-my-zsh" ]; then
-	cp -r $HOME/dotfile/ohmyzsh/custom $HOME/.oh-my-zsh
+	cp -r $HOME/dotfiles/ohmyzsh/custom $HOME/.oh-my-zsh
         cp $HOME/dotfiles/zshrc $HOME/.zshrc
         source $HOME/.zshrc
 fi
