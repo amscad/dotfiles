da285035995cfbba9fd18a177c74f2e5ebdafc9d
amscad <ajmansfield@gmail.com>
2019-12-24 14:28:55 +0000

    Copy custom themes and reload zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -138,3 +138,10 @@ fi
 
 # for current logged user
 sudo chsh -s /bin/zsh "$USER"
+
+# copy custom themes and plugins to .oh-my-zsh, if it has been installed
+if [ -d "$HOME/.oh-my-zsh" ]; then
+	cp -r ohmyzsh/custom $HOME/.oh-my-zsh
+        cp zshrc $HOME/.zshrc
+        source $HOME/.zshrc
+fi
