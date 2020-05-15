28d3f0ab03d13848eca1a0bffbf0124063299088
alex <ajmansfield@gmail.com>
2020-05-15 12:51:52 +0100

    Make sure in the correct folder when referencing custom themes

--- update-zsh.sh
+++ update-zsh.sh
@@ -1,6 +1,9 @@
 # copy custom themes and plugins to .oh-my-zsh, if it has been installed
 if [ -d "$HOME/.oh-my-zsh" ]; then
-	cp -r $HOME/dotfiles/ohmyzsh/custom $HOME/.oh-my-zsh
-        cp $HOME/dotfiles/zshrc $HOME/.zshrc
+  SCRIPT_DIR="${PWD}"
+  printf '%s\n' "$SCRIPT_DIR"
+
+  cp -r "$SCRIPT_DIR/ohmyzsh/custom" $HOME/.oh-my-zsh
+#        cp $HOME/dotfiles/zshrc $HOME/.zshrc
         source $HOME/.zshrc
 fi
