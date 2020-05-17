69ea0199ad79e8d2e86d68dce84fdeba687fbcd3
Alex <amscad@users.noreply.github.com>
2020-05-17 13:36:37 +0100

    Exclude gcloud for now, and update to use the PWD not home

--- ish/ish-setup.sh
+++ ish/ish-setup.sh
@@ -1,6 +1,6 @@
 # standard tools
 apk add python3 \
-  ruby \
+  	ruby \
 	vim \
 	vimdiff \
 	git \
@@ -17,13 +17,13 @@ apk add python3 \
 	man
 
 
-# google cloud shell
-# but only if it has not already been downloaded
-if [ ! -f "$HOME/google-cloud-sdk.tar.gz" ]; then
+## google cloud shell
+## but only if it has not already been downloaded
+#if [ ! -f "$HOME/google-cloud-sdk.tar.gz" ]; then
 #wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.0-linux-x86_64.tar.gz
-	curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.0-linux-x86_64.tar.gz -o $HOME/google-cloud-sdk.tar.gz
-	tar -zxvf $HOME/google-cloud-sdk.tar.gz  $HOME/google-cloud-sdk
-fi
+#	curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-274.0.0-linux-x86_64.tar.gz -o $HOME/google-cloud-sdk.tar.gz
+#	tar -zxvf $HOME/google-cloud-sdk.tar.gz  $HOME/google-cloud-sdk
+#fi
 
 # install ohmyzsh - a useful set of tools for zsh
 # but only if it has not been installed already
@@ -34,9 +34,9 @@ fi
 
 # copy custom themes and plugins to .oh-my-zsh, if it has been installed
 if [ -d "$HOME/.oh-my-zsh" ]; then
-	cp -r $HOME/dotfiles/ohmyzsh/custom $HOME/.oh-my-zsh
-        cp $HOME/dotfiles/zshrc $HOME/.zshrc
-        source $HOME/.zshrc
+	cp -r ${PWD}/ohmyzsh/custom $HOME/.oh-my-zsh
+    cp ${PWD}/zshrc $HOME/.zshrc
+    source $HOME/.zshrc
 fi
 
 echo "To complete the setup, navigate to the google-cloud-sdk folder and run './install.sh'"
