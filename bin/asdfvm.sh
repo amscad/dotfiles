803694ad38c9fefc4198493dc0d61462618eec63
alex <ajmansfield@gmail.com>
2024-09-24 08:20:23 +0100

    Only setup node

--- bin/asdfvm.sh
+++ bin/asdfvm.sh
@@ -9,13 +9,13 @@ SCRIPT_PATH=$(dirname "$(
 )")
 
 ###################################[ version manager ]########################
-# add asdf if it hasnt been added through homebrew
-echo "Configuring asdf version manager..."
-if [ ! -d "$HOME/.asdf" ]; then
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf 
-  append_to_zshrc "source $HOME/.asdf/asdf.sh" 1
-  append_to_zshrc "source $HOME/.asdf/completions/asdf.bash" 1
-fi
+# # add asdf if it hasnt been added through homebrew
+# echo "Configuring asdf version manager..."
+# if [ ! -d "$HOME/.asdf" ]; then
+#   git clone https://github.com/asdf-vm/asdf.git ~/.asdf 
+#   append_to_zshrc "source $HOME/.asdf/asdf.sh" 1
+#   append_to_zshrc "source $HOME/.asdf/completions/asdf.bash" 1
+# fi
 
 
 install_asdf_plugin() {
@@ -28,12 +28,12 @@ install_asdf_plugin() {
 }
 
 # shellcheck disable=SC1090
-source "$HOME/.asdf/asdf.sh"
-#install_asdf_plugin "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
+# source "$HOME/.asdf/asdf.sh"
+install_asdf_plugin "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
 #install_asdf_plugin "ruby" "https://github.com/asdf-vm/asdf-ruby.git"
 
-install_asdf_plugin "java" "https://github.com/halcyon/asdf-java.git"
-install_asdf_plugin "maven" "https://github.com/halcyon/asdf-maven"
+# install_asdf_plugin "java" "https://github.com/halcyon/asdf-java.git"
+# install_asdf_plugin "maven" "https://github.com/halcyon/asdf-maven"
 install_asdf_plugin "python" "https://github.com/danhper/asdf-python"
 #install_asdf_plugin "flutter" "https://github.com/oae/asdf-flutter.git"
 #install_asdf_plugin "dart" "https://github.com/patoconnor43/asdf-dart.git"
@@ -53,18 +53,23 @@ install_asdf_language() {
   fi
 }
 
+echo "Installing node"
+install_asdf_language "nodejs" "latest:20"
 
-echo "Installing Java 8"
-install_asdf_language "java" "latest:zulu-8"
+# echo "Installing Java 8"
+# install_asdf_language "java" "latest:zulu-8"
 
-echo "Installing Java 11"
-install_asdf_language "java" "latest:zulu-11"
+# echo "Installing Java 11"
+# install_asdf_language "java" "latest:zulu-11"
 
-echo "Installing Java 17"
-install_asdf_language "java" "latest:zulu-17"
+# echo "Installing Java 17"
+# install_asdf_language "java" "latest:zulu-17"
 
-echo "Installing Java latest"
-install_asdf_language "java" "latest:zulu"
+# echo "Installing Java latest"
+# install_asdf_language "java" "latest:zulu"
 
-echo "Installing latest Maven"
-install_asdf_language "maven" "latest"
+# echo "Installing latest Maven"
+# install_asdf_language "maven" "latest"
+
+echo "Installing latest python"
+install_asdf_language "python" "latest"
