5168268ec778f4dc0cd4ddd4f60d72029f7f4941
Alex <ajmansfield@gmail.com>
2024-09-29 12:34:55 +0100

    Fix the checks for the folders

--- bin/nvim.sh
+++ bin/nvim.sh
@@ -1,19 +1,17 @@
 # required
-if [ -f ~/.config/nvim ]; then
-    rm ~/.config/nvim
-elif [ ! -d ~/.config/nvim ]; then
-    mv ~/.config/nvim{,.bak}
+if [ -d ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim.bak
 fi 
 
 # optional but recommended
-if [ ! -d ~/.local/share/nvim ]; then
-    mv ~/.local/share/nvim{,.bak}
+if [ -d ~/.local/share/nvim ]; then
+    mv ~/.local/share/nvim ~/.local/share/nvim.bak
 fi 
-if [ ! -d ~/.local/state/nvim ]; then
-    mv ~/.local/state/nvim{,.bak}
+if [ -d ~/.local/state/nvim ]; then
+    mv ~/.local/state/nvim ~/.local/state/nvim.bak
 fi 
-if [ ! -d ~/.cache/nvim ]; then
-    mv ~/.cache/nvim{,.bak}
+if [ -d ~/.cache/nvim ]; then
+    mv ~/.cache/nvim ~/.cache/nvim.bak
 fi 
 
 git clone https://github.com/LazyVim/starter ~/.config/nvim
